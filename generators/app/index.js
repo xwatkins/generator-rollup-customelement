2072cd88cb598e5c2ac6e6e86f78d35a97916fac
Xavier Watkins <xwatkins@ebi.ac.uk>
2017-10-04 10:46:27 +0100

    Adding gitignore

--- generators/app/index.js
+++ generators/app/index.js
@@ -68,6 +68,10 @@ module.exports = class extends Generator {
       this.destinationPath('.babelrc')
     );
     this.fs.copy(
+      this.templatePath('_.gitignore'),
+      this.destinationPath('.gitignore')
+    );
+    this.fs.copy(
       this.templatePath('_.eslintrc.json'),
       this.destinationPath('.eslintrc.json')
     );
